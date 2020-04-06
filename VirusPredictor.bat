f18c42d2359849cdc49b861c577bd276a0ab4bff
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-04-06 16:16:03 -0400

    Creates a "recording" of the virus

--- VirusPredictor.bat
+++ VirusPredictor.bat
@@ -6,7 +6,8 @@ mode 475
 cls
 :nt1
 cls
-echo COVID-19 is spreading. 
+echo COVID-19 is spreading. NOTE: This virus stimulator 
+set /p vsn=Virus Stimulation Name:
 set /p Initial_Population=Initial Population:
 set /p infectantsperday=People Infected Per day:
 set /p deathrec=Recover from death Rate:
@@ -17,6 +18,7 @@ set /p initialinfectants=Initial Infectaants:
 set /p init_deaths=Initial Deaths:
 set /p init_cure=Initial Cure percent made:
 set /p death_per_day=Deaths per day:
+echo This text is required for this file to function properly > %CD%\viruses_tests\%vsn%.txt
 :: set 1=2
 :: not set 1 = 2
 echo First:
@@ -30,6 +32,7 @@ echo Cure percent made: %curepercentadv%
 pause
 cls
 : nxt
+set /a day=%day%+1
 
 set /a infectants=%infectants%+%infectantsperday%/%recoverrate%
 
@@ -38,11 +41,23 @@ set /a deaths=%deaths%+%death_per_day%/%deathrec%
 set /a cure=%cure%+%curepercentadv%
 
 set /a inf=%Initial_Population%-%inaffectable%
-
+echo By Mayank, user is %username%
+echo Day %day% of surviving %vsn% 
+echo Virus Stimulation Name: %vsn%
 echo Affectable People: %inf%
 echo Infectants: %infectants%
 echo Deaths: %deaths%
 echo Cure percent made: %cure%
+:: create file
+:startofcreatefile
+set tmpvar=12
+echo %vsn% >> %vsn%.txt
+echo Day %day% of surviving %vsn%  >> %CD%\viruses_tests\%vsn%.txt
+echo There are %inf% affectable people >> %CD%\viruses_tests\%vsn%.txt
+echo There are %infectants% infectants >> %CD%\viruses_tests\%vsn%.txt
+echo There are %deaths% deaths >> %CD%\viruses_tests\%vsn%.txt
+echo Cure is %cure% percent made >> %CD%\viruses_tests\%vsn%.txt
+
 pause
 cls
 goto nxt
