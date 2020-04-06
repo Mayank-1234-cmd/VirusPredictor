1fe8d1ced4e57af31267672051eb9bedda586044
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-04-06 16:37:19 -0400

    in dev

---
+++ covidinfo.bat
@@ -0,0 +1,14 @@
+@echo off
+title COVID-19
+cls
+echo CO
+timeout /t "1" >nul
+echo COVI
+timeout /t "1" >nul
+echo COVID-
+timeout /t "1" >nul
+echo COVID-19
+timeout /t "1" >nul
+echo COVID-19 i
+timeout /t "0.5" >nul
+echo COVID-19 is 
